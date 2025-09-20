1e70f9a7a57eea2ac581610ca1f9448423e13c3c
Efrem <rensi.efrem@gmail.com>
2025-09-19 17:22:43 -0700

    conclude bluetooth fix

--- base.nix
+++ base.nix
@@ -199,24 +199,23 @@
     powerOnBoot = true;
     settings = {
       General = {
-        # Shows battery charge of connected devices on supported
-        # Bluetooth adapters. Defaults to 'false'.
+        # Shows battery charge of connected devices on supported Bluetooth adapters.
         Experimental = true;
         # When enabled other devices can connect faster to us, however
-        # the tradeoff is increased power consumption. Defaults to
-        # 'false'.
+        # the tradeoff is increased power consumption.
         FastConnectable = true;
       };
       Policy = {
         # Enable all controllers when they are found. This includes
         # adapters present on start as well as adapters that are plugged
-        # in later on. Defaults to 'true'.
+        # in later on.
         AutoEnable = true;
       };
     };
   };
 
-  # Enable sound with pipewire.
+  # Enable sound with pipewire (not Pulse Audio).
+  # https://wiki.nixos.org/wiki/PipeWire
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
