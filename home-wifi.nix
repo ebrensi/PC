99adf8b44afe13215126328c7c2814d27593464d
Efrem Rensi <BarefootEfrem@gmail.com>
2025-10-07 15:25:53 -0700

    update wifi

--- home-wifi.nix
+++ home-wifi.nix
@@ -4,25 +4,53 @@
   lib,
   ...
 }: {
-  networking.wireless = {
-    enable = true;
-    userControlled.enable = true;
-    networks.CiscoKid = {
-      ssid = "CiscoKid";
-      pskRaw = "8c1b86a16eecd3996e724f7e21ff1818b03c8c463457fc9a3901c5ef7bc14d55";
-    };
-  };
   networking.networkmanager = {
     enable = true;
     wifi.powersave = true;
-    settings = {
-      # Prefer wifi over wired ethernet when both are available
-      # since wired connection is a relatively slow Powerline connection
-      connection-wifi = {
-        match-device = "type:wifi";
-        "ipv4.route-metric" = 0;
-        "ipv6.route-metric" = 0;
+
+    # Define the wifi connection
+    ensureProfiles = {
+      environmentFiles = [
+        (pkgs.writeText "nm-home-wifi.env" ''
+          WIFI_PSK=8c1b86a16eecd3996e724f7e21ff1818b03c8c463457fc9a3901c5ef7bc14d55
+        '')
+      ];
+      profiles = {
+        CiscoKid = {
+          connection = {
+            id = "CiscoKid";
+            type = "wifi";
+            autoconnect = true;
+          };
+          wifi = {
+            ssid = "CiscoKid";
+            mode = "infrastructure";
+          };
+          wifi-security = {
+            key-mgmt = "wpa-psk";
+            psk = "$WIFI_PSK";
+          };
+          ipv4 = {
+            method = "auto";
+            route-metric = 0;
+          };
+          ipv6 = {
+            method = "auto";
+            route-metric = 0;
+          };
+        };
       };
     };
+
+    # # Set higher metrics for wired connections
+    # settings = {
+    #   # Prefer wifi over wired ethernet when both are available
+    #   # since wired connection is a relatively slow Powerline connection
+    #   connection-wifi = {
+    #     match-device = "type:wifi";
+    #     "ipv4.route-metric" = 0;
+    #     "ipv6.route-metric" = 0;
+    #   };
+    # };
   };
 }
