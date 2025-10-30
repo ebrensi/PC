18aaadac2115bad363807eccabe28975f9a7723f
Efrem <rensi.efrem@gmail.com>
2025-10-29 21:33:32 -0700

    increase download buffer size

--- base.nix
+++ base.nix
@@ -247,6 +247,7 @@
 
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
+    download-buffer-size = 524288000;
   };
   nix.nixPath = ["nixpkgs=${pkgs.path}"];
   system.stateVersion = "25.05";
