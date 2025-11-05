731f9bc503ae75848014d80ccd9e4d6338284bbc
Efrem <rensi.efrem@gmail.com>
2025-11-05 11:39:23 -0800

    add cachix

--- base.nix
+++ base.nix
@@ -247,6 +247,8 @@
 
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
+    substituters = ["https://nix-community.cachix.org"];
+    trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     download-buffer-size = 524288000;
   };
   nix.nixPath = ["nixpkgs=${pkgs.path}"];
