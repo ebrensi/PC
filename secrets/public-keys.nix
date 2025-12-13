1c526fabfc615b7bf9ed7db88f0764e3400b9b3b
Efrem <rensi.efrem@gmail.com>
2025-12-13 15:08:29 -0800

    update phone public key

--- secrets/public-keys.nix
+++ secrets/public-keys.nix
@@ -3,5 +3,5 @@
   personal-ssh-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINXFGk5SnYLScFm7rmFUhx36jaYX0sol85ajQczJvMj+ BarefootEfrem@gmail.com";
   AP-ssh-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII//cI1RPUk4caXbGHdMJpQB7VuydedUCP/Kt9mALxVY barefootefrem@gmail.com";
   home-cache-key = "home-nix-cache:I3FHReN9xjiuhJGMlSk0eKvdOiqoBWZHNpKHdIeXxiQ=";
-  phone = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQa/BhPVVlakP4OBW1FOfq1PbnF585gNkGBO49/Z2fI pixel9-linux-terminal";
+  phone = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGGYaTdAHHj3wfQWZEbxYiCI1bDCjOuzpdsML9M/7JVA efrem-pixel9";
 }
