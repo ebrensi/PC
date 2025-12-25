4fa40d0f81878e9e65ec48c819c7a75388d368b6
Efrem <rensi.efrem@gmail.com>
2025-12-25 08:01:49 -0800

    update home-server key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  home-server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINjqqggoXpaVqylXWGG1myX89SZeYpWkM78w+4t350TJ root@adder-ws";
+  home-server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDLW9b8xLcTO+g31JJJEmzVOgLP4tKmQ2UVYXftGLYzw root@adder-ws";
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHA+n6JXJc1/RzoiDdKswMZL7toAQurB7lRULXUGJ4PS root@thinkpad";
   m1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG2+sxwrvNQvq0mzJIu/c6mwDadnLcDczBd3CyAZ6p9U root@m1";
   user-efrem = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINXFGk5SnYLScFm7rmFUhx36jaYX0sol85ajQczJvMj+ BarefootEfrem@gmail.com";
