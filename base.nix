04908c0e34c4651db42a8575ea9ace69b87a4ec4
Efrem <rensi.efrem@gmail.com>
2025-12-26 23:53:01 -0800

    go back

--- base.nix
+++ base.nix
@@ -245,7 +245,7 @@
   };
 
   nix.settings = {
-    experimental-features = ["nix-command" "flakes" "impure-derivations" "ca-derivations"];
+    experimental-features = ["nix-command" "flakes"];
     substituters = ["https://nix-community.cachix.org"];
     trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     download-buffer-size = 524288000;
