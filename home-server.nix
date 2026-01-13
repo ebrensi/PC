9480ba9fd7c7ea318ac68ba45a55a2bcbb3299ac
Efrem <rensi.efrem@gmail.com>
2026-01-12 18:11:55 -0800

    try again

--- home-server.nix
+++ home-server.nix
@@ -125,7 +125,7 @@
   # Enable exFAT filesystem support for this USB drive
   boot.supportedFilesystems = ["exfat"];
 
-  services.avahi.allowInterfaces = ["wlp0s20f3"];
+  services.avahi.denyInterfaces = ["eno0"];
 
   # This is a laptop machine acting as a server so we don't want it to sleep
   # When hooked to a dock or external power
