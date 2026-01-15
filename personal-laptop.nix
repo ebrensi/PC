cd8710e836688b6b6c273a32ad8abdd53e161e8c
Efrem <rensi.efrem@gmail.com>
2026-01-15 15:47:15 -0800

    update endpoint

--- personal-laptop.nix
+++ personal-laptop.nix
@@ -74,8 +74,8 @@
         publicKey = "qtyeOtl/yxdpsELc8xdcC6u0a1p+IZU0HwHrHhUpGxc=";
         # Route all VPN traffic (IPv6 only) through relay
         allowedIPs = ["12.167.1.0/24" "fd42:af9e:1c7d:8b3a::/64"];
-        # endpoint = "73.15.57.26:51820"; # Public IP for roaming
-        endpoint = "t2.local:51820";
+        endpoint = "73.15.57.26:51820"; # Public IP for roaming
+        # endpoint = "t2.local:51820";
         persistentKeepalive = 25;
       }
       # {
