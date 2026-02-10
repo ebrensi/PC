f3aa96c7f1fc6cd5f4e1f959ca6194be1a5f7b55
Efrem <rensi.efrem@gmail.com>
2026-02-10 15:49:41 -0800

    get rid of download-buffer setting

--- base.nix
+++ base.nix
@@ -193,7 +193,6 @@
     experimental-features = ["nix-command" "flakes"];
     substituters = ["https://nix-community.cachix.org"];
     trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-    download-buffer-size = 524288000;
   };
   nix.nixPath = ["nixpkgs=${pkgs.path}"];
   system.stateVersion = "25.05";
