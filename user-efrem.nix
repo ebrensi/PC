55edfaddd6a6e5eb95b09e212b9e3db46f231015
Efrem <rensi.efrem@gmail.com>
2026-02-28 20:29:30 -0800

    improve tmux config

--- user-efrem.nix
+++ user-efrem.nix
@@ -133,8 +133,77 @@ in {
         set -as terminal-features ',tmux*:clipboard'
         set -s copy-command 'xsel -i'
 
+        set-option -g set-titles on
+        set-option -g set-titles-string "#{pane_title}"
+
+        bind c new-window -c "#{pane_current_path}"
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+
+        # Styling ##
+        # gruvbox material colorscheme (mostly)
+        RED="#ea6962"
+        GREEN="#a9b665"
+        YELLOW="#d8a657"
+        BLUE="#7daea3"
+        MAGENTA="#d3869b"
+        CYAN="#89b482"
+        BLACK="#1d2021"
+        DARK_GRAY="#32302F"
+        LIGHT_GRAY="#4F4946"
+        BG="#32302F"
+        FG="#d4be98"
+
+        # Nerdfont characters
+        HALF_ROUND_OPEN="#(printf '\uE0B6')"
+        HALF_ROUND_CLOSE="#(printf '\uE0B4')"
+        TRIANGLE_OPEN="#(printf '\uE0B2')"
+        TRIANGLE_CLOSE="#(printf '\uE0B0')"
+
+        # Basic colors of the Statusbar
+        set-option -g status-style bg=''${BG},fg=''${FG}
+
+        # Show the window list centered between the left and the right section
+        set-option -g status-justify centre
+
+        # Style and set contents on the left section
+        set-option -g status-left "\
+        #[fg=''${LIGHT_GRAY},bg=default]''${HALF_ROUND_OPEN}\
+        #[bg=''${LIGHT_GRAY},fg=''${YELLOW}]#S \
+        #[fg=''${LIGHT_GRAY},bg=default]''${TRIANGLE_CLOSE}\
+        "
+
+        # Style and set contents on the right section
+        set-option -g status-right "\
+        #[fg=''${LIGHT_GRAY},bg=default]''${TRIANGLE_OPEN}\
+        #[bg=''${LIGHT_GRAY},fg=''${CYAN}] #h\
+        #[fg=''${LIGHT_GRAY},bg=default]''${HALF_ROUND_CLOSE}\
+        "
+
+        # Set max length of left and right section
+        set-option -g status-left-length 100
+        set-option -g status-right-length 100
+
+        # Style and set content for the inactive windows
+        set-option -g window-status-format "\
+        \
+        #I\
+        #[fg=''${MAGENTA}]:\
+        #[fg=default]#W\
+        \
+        "
+
+        # Style and set content for the active windows
+        set-option -g window-status-current-format "\
+        #[fg=''${LIGHT_GRAY},bg=default]''${HALF_ROUND_OPEN}\
+        #[bg=''${LIGHT_GRAY},fg=default]#I\
+        #[fg=''${RED}]:\
+        #[fg=default]#W\
+        #[fg=''${LIGHT_GRAY},bg=default]''${HALF_ROUND_CLOSE}\
+        "
+
         # Set status-right BEFORE plugins load so they can interpolate the variables
-        set -g status-right "CPU: #{cpu_percentage} | %H:%M"
+        set -g status-right "#(hostname) #{cpu_percentage} CPU | %H:%M"
       '';
     };
 
